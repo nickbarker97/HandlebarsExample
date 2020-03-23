e8f6b60f1a8a8ac5ca0f6be1986160db70b3b70c
Admin <admin@Admins-MacBook.local>
2020-03-23 12:15:29 +0000

    added basic code for Handlebars

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
+var handlebars = require('express-handlebars');
 
+
+app.set('view engine','hbs');
+app.engine('hbs',handlebars({
+layoutDir: __dirname + '/views/layouts',
+extname: 'hbs'
+}));
 app.get('/', (req, res)=>{
-    res.send('This is the Home Page')
+    res.render('index', {layout: 'main'});
 });
 
 app.get('/about', (req, res)=>{
-    res.send('This is the About page')
+    res.render('about', {layout: 'main'});
 });
 
 app.get('/contact', (req, res)=>{
